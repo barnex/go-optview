538b7b73795812c31cce13e6167df8ca98d931c4
Arne Vansteenkiste <barnex@gmail.com>
2012-03-11 13:48:47 +0100

    some cleanup

--- main.go
+++ main.go
@@ -16,34 +16,15 @@ var (
 
 func main() {
 	flag.Parse()
-	readFrom(os.Stdin)
+	ReadCompilerOutput(os.Stdin)
 
 	for n, f := range files {
-		fmt.Println(`//`, n, ":")
-
-		in_, err := os.Open(n)
-		if err != nil {
-			stderr(err)
-			continue
-		}
-		in := bufio.NewReader(in_)
-
-		lineNo := 1
-		for l, prefix, err := in.ReadLine(); err == nil; l, prefix, err = in.ReadLine() {
-			if prefix {
-				panic("Enlarge your buffer!")
-			}
-			fmt.Print(string(l))
-			if opts, ok := f.Opts[lineNo]; ok {
-				fmt.Print("//", opts)
-			}
-			fmt.Println()
-			lineNo++
-		}
+		fmt.Fprintln(os.Stdout, "//", n, ":")
+		f.FormatTo(os.Stdout)
 	}
 }
 
-func readFrom(in_ io.Reader) {
+func ReadCompilerOutput(in_ io.Reader) {
 	in := bufio.NewReader(in_)
 	for l, prefix, err := in.ReadLine(); err == nil; l, prefix, err = in.ReadLine() {
 		if prefix {
@@ -58,17 +39,17 @@ func parseLine(line string) {
 	defer func() {
 		err := recover()
 		if err != nil {
-			stderr(`optview:`, err, ` while parsing "`, line, `"`)
+			Stderr(`optview:`, err, ` while parsing "`, line, `"`)
 		}
 	}()
 	words := strings.SplitN(line, ":", 3)
 	filename := words[0]
 	lineNo := Atoi(words[1])
 	info := words[2]
-	Source(filename).AddOpt(lineNo, info)
+	GetSourceFile(filename).AddOpt(lineNo, info)
 }
 
-func stderr(msg ...interface{}) {
+func Stderr(msg ...interface{}) {
 	fmt.Fprintln(os.Stderr, msg...)
 }
 
@@ -80,7 +61,29 @@ func Atoi(a string) int {
 	return i
 }
 
-func Source(fileName string) *SourceFile {
+func (f *SourceFile) FormatTo(out io.Writer) {
+	in_, err := os.Open(f.Name)
+	if err != nil {
+		Stderr(err)
+		return
+	}
+	in := bufio.NewReader(in_)
+
+	lineNo := 1
+	for l, prefix, err := in.ReadLine(); err == nil; l, prefix, err = in.ReadLine() {
+		if prefix {
+			panic("Enlarge your buffer!")
+		}
+		fmt.Fprint(out, string(l))
+		if opts, ok := f.Opts[lineNo]; ok {
+			fmt.Print("//", opts)
+		}
+		fmt.Fprintln(out)
+		lineNo++
+	}
+}
+
+func GetSourceFile(fileName string) *SourceFile {
 	if file, ok := files[fileName]; ok {
 		return file
 	}
