6ed7d0b23ef12aefe0e674592d76676fbb0d2f20
Arne Vansteenkiste <barnex@gmail.com>
2012-03-11 13:23:50 +0100

    source file map

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: *.go
 	gofmt -w *.go
-	go tool 6g -m -o optview.6 *.go
-	go tool 6l -m -o optview optview.6
+	go tool 6g -o optview.6 *.go
+	go tool 6l -o optview optview.6
 
--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
+)
+
+var (
+	files map[string]*SourceFile = make(map[string]*SourceFile)
 )
 
 func main() {
 	flag.Parse()
 	readFrom(os.Stdin)
+
+	for f := range files {
+		fmt.Println(`//`, f, ":")
+	}
 }
 
 func readFrom(in_ io.Reader) {
@@ -30,9 +39,42 @@ func parseLine(line string) {
 		}
 	}()
 	words := strings.SplitN(line, ":", 3)
-	fmt.Println(words)
+	filename := words[0]
+	lineNo := Atoi(words[1])
+	info := words[2]
+	Source(filename).AddOpt(lineNo, info)
 }
 
 func stderr(msg ...interface{}) {
 	fmt.Fprintln(os.Stderr, msg...)
+}
+
+func Atoi(a string) int {
+	i, err := strconv.Atoi(a)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func Source(fileName string) *SourceFile {
+	if file, ok := files[fileName]; ok {
+		return file
+	}
+	file := NewSourceFile(fileName)
+	files[fileName] = file
+	return file
+}
+
+func NewSourceFile(fileName string) *SourceFile {
+	return &SourceFile{fileName, make(map[int][]string)}
+}
+
+type SourceFile struct {
+	Name string
+	Opts map[int][]string
+}
+
+func (this *SourceFile) AddOpt(line int, opt string) {
+	this.Opts[line] = append(this.Opts[line], opt)
 }
